68d9303beae56cb441882bfa05cfb4c668bb2b4d
Saksham Garg <saksham.garg14509@gmail.com>
2024-03-29 18:18:03 +0530

    heheheheeh
    chal gaya mkcccc

--- models/product.js
+++ models/product.js
@@ -4,28 +4,28 @@ const Schema = mongoose.Schema;
 const productSchema = new mongoose.Schema({
     title: {
         type: String,
-        required: true
+        // required: true
     },
     description: {
         type: String,
-        required: true
+        // required: true
     },
     category: {
         type: String,
-        required: true
+        // required: true
     },
     image: {
         type: String,
-        required: true
+        // required: true
     },
     price: {
         type: Number,
-        required: true
+        // required: true
     },
-    contactNumber: {
-        type: String,
-        required: true
-    },
+    // contactNumber: {
+    //     type: String,
+    //     required: true
+    // },
     user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }, // Reference to the user who posted the product
     forum: { type: mongoose.Schema.Types.ObjectId, ref: 'Forum' } // Reference to the forum where the product is posted
 });
