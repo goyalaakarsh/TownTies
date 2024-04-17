ce86ec8c94a5708af0c1b51a0e26eac92744022b
Saksham Garg <saksham.garg14509@gmail.com>
2024-04-17 22:59:28 +0530

    le
    tu bhi le

--- init/mart-data.js
+++ init/mart-data.js
@@ -1,35 +1,37 @@
 const productsData = [
     {
-        title: "Product 1",
-        description: "Description of Product 1",
-        category: "Category 1",
+        title: "Lord of The Rings Bookset",
+        description: "Complete bookset of the Trilogy \"Lord of The Rings \" written by J. R. R. Tolkien. Bought it a year ago. Must read series. Books are in near-new condition",
+        category: "Books and Media",
         image: {
-            data: "https://images.unsplash.com/photo-1522771739844-6a9f6d5f14af?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-            contentType: "image/jpeg"
+            data: "https://imgs.search.brave.com/ZhAUkC8cVI9A6zvmAK9bnU5WAhMxGoRFgLGOjJyy_Os/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9waWN0/dXJlcy5hYmVib29r/cy5jb20vaW52ZW50/b3J5L21kL21kMzA3/ODQ4NjQ1ODAuanBn",
+            filename: "LOTR Bookset/jpeg"
         },
-        price: 100,
+        price: 800,
         contactNumber: "1234567890"
     },
     {
-        title: "Product 2",
-        description: "Description of Product 2",
-        category: "Category 2",
+        title: "Wilson Lawn Tennis Racquet",
+        description: "Mildly used wilson lawn tennis racquet. The strings were recently changed and are geared to aid with power in each hit.",
+        category: "Sports and Outdoor Gear",
         image: {
-            data: "https://images.unsplash.com/photo-1522771739844-6a9f6d5f14af?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-            contentType: "image/jpeg"
+            data: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fcdnmedia.racquets4u.com%2Fmedia%2Fiopt%2Fcatalog%2Fproduct%2Fcache%2Fcd3f430ba3d61418b233993754ed59dd%2Fr%2Fo%2Froland_garros_blade_98_18x20_v8_tennis_racket_-_1.webp&tbnid=fHx774MFgZgEDM&vet=12ahUKEwjWs-Ha4ZyFAxWbhGMGHcrrCs4QMygBegUIARCwAQ..i&imgrefurl=https%3A%2F%2Fwww.racquets4u.com%2Ftennis%2Ftennis-racquets%2Fintermediate.html&docid=rDuxhvVbgkxrqM&w=250&h=250&q=wilson%20tennis%20rackets%20for%20intermediate%20players&ved=2ahUKEwjWs-Ha4ZyFAxWbhGMGHcrrCs4QMygBegUIARCwAQ",
+            filename: "wilson racquet for intermmediate players/jpeg"
         },
-        price: 200,
+        price: 9200,
         contactNumber: "9876543210"
     },
     {
-        title: "Product 3",
-        description: "Description of Product 3",
-        category: "Category 3",
+        title: "Cort AD810",
+        description: "Mildly used Cort AD810. It has a sprucewood top with Mahogony back and sides. Equipped with D'Addario Gauge 0.12 string",
+        category: "Music",
         image: {
-            data: "https://images.unsplash.com/photo-1522771739844-6a9f6d5f14af?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-            contentType: "image/jpeg"
+            data: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSuvVYJNSLfML2u7e06jFXnXZlhG_aln_lOhg&usqp=CAU",
+            contentType: "CortAD810/jpeg"
         },
-        price: 300,
+        price: 6300,
         contactNumber: "5555555555"
     }
 ];
+
+module.exports = martdata
---
+++ init/sample-forum.js
@@ -0,0 +1,9 @@
+const sampleforumdata = {
+    name: "Dummy Forum",
+    icon: {
+        url: "https://static.thenounproject.com/png/1526832-200.png",
+        filename: "Default forum icon.png"
+    },
+}
+
+module.exports = sampleForum;
