1b6a14f39b1b828e4a5bfd688e4007efb0378c2c
Aakarsh Goyal <aakarshgoyal23@gmail.com>
2024-04-21 02:39:15 +0530

    .

--- app.js
+++ app.js
@@ -30,39 +30,7 @@ const sampleForum = require("./init/sample-forum.js");
 const martData = require("./init/mart-data.js");
 const flash = require("connect-flash")
 const bodyParser = require("body-parser");
-const http = require('http');
-const socketIO = require('socket.io');
-const server = http.createServer(app);
-const Chat = require('./models/chat.js');
 
-const io = socketIO(server); 
-
-io.on('connection', (socket) => {
-    console.log('A user connected');
-
-    // Receive chat messages from clients
-    socket.on('send', async (data) => {
-        console.log('Received chat message:', data);
-
-        // Save the message to your database
-        try {
-            console.log("hi");
-            const newChatMessage = new Chat({
-                user: data.sender,
-                message: data.message,
-                // Add any other necessary fields
-            });
-            console.log("hi");
-            await newChatMessage.save();
-            console.log("hi");
-
-            // Broadcast the message to all clients
-            socket.broadcast.emit('recieve', data); // You can refine this to only emit to specific rooms or namespaces if needed
-        } catch (err) {
-            console.error('Error saving chat message:', err);
-        }
-    });
-});
 
 const PORT = process.env.PORT || 3001;
 server.listen(PORT, () => {
